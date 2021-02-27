baab73b38c1ba2d63d9d043f60e9348547198086
Lionnel Patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2021-02-27 10:23:10 +0000

    try with buikdpack on heroku

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -10,7 +10,7 @@ module Api
                         "type": "git",
                         "url": "https://github.com/lionnelhost/api_test.git"
                       },
-                    "author": "Patrick",
+                    "author": "Patrick Lionnel",
                     "license": "MIT"
                 }
 
