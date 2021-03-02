a16a4ff58b42b838fb93161c7dbccb7fa1babc41
Lionnel Patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2021-03-02 20:19:50 +0000

    remove github action ci

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
-      get '/home' => "home#index"
+      get '/' => "home#index"
       
       # mount_devise_token_auth_for 'User', at: 'auth' 
 
@@ -17,5 +18,5 @@ Rails.application.routes.draw do
   end
   
 
-  root to: "home#index"
+  
 end
